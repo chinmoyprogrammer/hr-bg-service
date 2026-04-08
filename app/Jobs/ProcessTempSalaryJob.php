26dd2fc47663a74e629f860e9c053e46edba37bc
chinmoyprogrammer <chinmoyprogrammer@gmail.com>
2026-04-08 11:43:58 +0600

    # Temp salary process modified, # Model added

--- app/Jobs/ProcessTempSalaryJob.php
+++ app/Jobs/ProcessTempSalaryJob.php
@@ -34,13 +34,17 @@ class ProcessTempSalaryJob extends Job implements ShouldQueue
 
     public function handle(): void
     {
+        $payload = $this->payload;
+        Log::info('Ki re vai!!!', ['payload' => $payload]);
         $PayrollPreSalarySheetDeduction = [];
         $PayrollSalaryAdvanceLoanNOtherInstallment = [];
         $PayrollSalarySheetHeads = [];
-        User::select('id')
-            ->with(['hasOfficialInformation','loans' => function($query){
+
+
+        User::with(['hasOfficialInformation','loans' => function($query){
                 $query->where('is_fully_paid', 0);
             }])
+            ->whereHas('hasOfficialInformation')
             ->where('status', 1)
             ->where('user_type_id', 1) // 1 = employee
             ->where('is_draft', 0)
@@ -48,56 +52,64 @@ class ProcessTempSalaryJob extends Job implements ShouldQueue
             ->lazy()
             ->each(function ($user) use(&$PayrollPreSalarySheetDeduction, &$PayrollSalaryAdvanceLoanNOtherInstallment, &$PayrollSalarySheetHeads)
             {
-
+                // Log::info('Line 52', ['payload' => $user]);
                 if($user->loans->count() > 0)
                 {
                     foreach($user->loans as $loan){
+                        // Log::info('Line 56', ['payload' => var_dump($loan)]);
+                        // Log::info('Line 57', ['payload' => $user->loans->count()]);
+                        // Log::info('Line 58', ['payload' => $user->loans]);
                         //.... process loan installments ( user can make a request for pausing the installment deduction for the month, need a history/ settings table )
-
-                        // insert data to payroll_pre_salary_sheet_deductions table
-                        $deduct_loan_this_month =1;
-                        if($deduct_loan_this_month)
+                        if($loan != null && gettype($loan) == 'object')
                         {
-                            // PayrollPreSalarySheetDeduction::create([
-                            //     'child_data_identifier_key_incoming' => 'payroll_salary_advance_n_loans_'.$loan->id,
-                            //     'amount' => $loan->primary_installment_amount,
-                            //     'type' => 'loan',
-                            //     'created_at' => date('Y-m-d H:i:s'),
-                            // ]);
-                            $PayrollPreSalarySheetDeduction[] = [
-                                'child_data_identifier_key_incoming' => 'payroll_salary_advance_n_loans_'.$loan->id,
-                                'amount' => $loan->primary_installment_amount,
-                                'type' => 'loan',
-                                'created_at' => date('Y-m-d H:i:s'),
-                            ];
 
-                            // insert data to PayrollSalaryAdvanceLoanNOtherInstallments
-                            // Use Eloquent model instead of raw DB insert for consistency
-                            // PayrollSalaryAdvanceLoanNOtherInstallment::create([
-                            //     'payroll_salary_advance_n_loan_id' => $loan->id,
-                            //     'coa_id' => 0,
-                            //     'amount' => $loan->primary_installment_amount,
-                            //     'remaining_amount' => $loan->outstanding_balance,
-                            //     'adjustment_status' => 'with_salary',
-                            //     'is_bad_debt' => 0,
-                            // ]);
-                            $PayrollSalaryAdvanceLoanNOtherInstallment[] = [
-                                'payroll_salary_advance_n_loan_id' => $loan->id,
-                                'coa_id' => 0,
-                                'amount' => $loan->primary_installment_amount,
-                                'remaining_amount' => $loan->outstanding_balance,
-                                'adjustment_status' => 'with_salary',
-                                'is_bad_debt' => 0,
-                            ];
+                        
+                            // insert data to payroll_pre_salary_sheet_deductions table
+                            $deduct_loan_this_month =1;
+                            if($deduct_loan_this_month)
+                            {
+                                // PayrollPreSalarySheetDeduction::create([
+                                //     'child_data_identifier_key_incoming' => 'payroll_salary_advance_n_loans_'.$loan->id,
+                                //     'amount' => $loan->primary_installment_amount,
+                                //     'type' => 'loan',
+                                //     'created_at' => date('Y-m-d H:i:s'),
+                                // ]);
+                                $PayrollPreSalarySheetDeduction[] = [
+                                    'child_data_identifier_key_incoming' => 'payroll_salary_advance_n_loans_'.$loan->id,
+                                    'amount' => $loan->primary_installment_amount,
+                                    'type' => 'loan',
+                                    'created_at' => date('Y-m-d H:i:s'),
+                                ];
 
-                            //...todo::this update should be made after final salary generation
-                            // $loan->outstanding_balance -= $loan->primary_installment_amount;
-                            // $loan->is_fully_paid = $loan->outstanding_balance == 0? 1 : 0;
-                            // $loan->save();
+                                // insert data to PayrollSalaryAdvanceLoanNOtherInstallments
+                                // Use Eloquent model instead of raw DB insert for consistency
+                                // PayrollSalaryAdvanceLoanNOtherInstallment::create([
+                                //     'payroll_salary_advance_n_loan_id' => $loan->id,
+                                //     'coa_id' => 0,
+                                //     'amount' => $loan->primary_installment_amount,
+                                //     'remaining_amount' => $loan->outstanding_balance,
+                                //     'adjustment_status' => 'with_salary',
+                                //     'is_bad_debt' => 0,
+                                // ]);
+                                $PayrollSalaryAdvanceLoanNOtherInstallment[] = [
+                                    'payroll_salary_advance_n_loan_id' => $loan->id,
+                                    'coa_id' => 0,
+                                    'amount' => $loan->primary_installment_amount,
+                                    'remaining_amount' => $loan->outstanding_balance,
+                                    'adjustment_status' => 'with_salary',
+                                    'is_bad_debt' => 0,
+                                ];
 
+                                //...todo::this update should be made after final salary generation
+                                // $loan->outstanding_balance -= $loan->primary_installment_amount;
+                                // $loan->is_fully_paid = $loan->outstanding_balance == 0? 1 : 0;
+                                // $loan->save();
+
+                            }
                         }
                     }
                 }
+                // Log::info('Line 103', ['payload' => $user]);
                 //.... process meal cost
                 //prent days x meal cost = meal cost deduction
                 //insert into payroll_pre_salary_sheet_deductions table
@@ -147,33 +159,22 @@ class ProcessTempSalaryJob extends Job implements ShouldQueue
 
 
 
-                //.... process and gather other regular salary components(like basic salary, allowances, deductions(attendance related), etc.)
-                    //get Payroll salary heads ( individual employee employee salary heads like: basic, medical, House rent etc)
-                    //.... get salary components data of this employee
-                    $head_amounts = PayrollSalaryHead::where('status', 1)->get();
-
-                    // loop through each head
-                    //.... insert salary to TempSalary
-                    foreach($head_amounts as $head)
-                    {
-                        $PayrollSalarySheetHeads[] = [
-                            'salary_sheet_temp_id' => $this->payload['salary_sheet_temp_id'] ?? null,
-                            'head_id' => $head->salary_head_id,
-                            'value' => $head->amount,
-                            'is_earning' => $head->is_earning,
-                            'created_user_id' => 1, // todo:: need a system user id
-                            'created_at' => date('Y-m-d H:i:s'),
-                        ];
-                    }
 
 
                     //..... calculate absent deductions
 
-                    LateAttendanceRecord::where('user_id', $user->id)
-                        ->where('attendance_date', $this->payload['attendance_date'])
-                        ->update([
-                            'is_late' => 1,
-                        ]);
+                    //
+                    $lateAttendanceRecord = LateAttendanceRecord::where('employee_user_id', $user->id)
+                        ->where('attendance_date', $this->payload['attendance_date']) //todo:: ??? why date ? when calculating for whole month
+                        ->where('month', $this->payload['month'] ?? date('m'))
+                        ->where('year', $this->payload['year'] ?? date('Y'))
+                        ->where('deduction_status', 'Pending')
+                        ->where('calculated_deduction', '>',0)
+                        ->whereNull('deleted_by')
+                        ->whereNull('deleted_at')
+                        ->selectRaw('SUM(applied_deduction) as total_applied_deduction, COUNT(*) as total_records')
+                        ->first();
+                        ;
 
                     //...... calculate late deductions
 
@@ -183,16 +184,16 @@ class ProcessTempSalaryJob extends Job implements ShouldQueue
                     //deduction type -> 'loan','pr','absent','pf','meal','late','ait', 'ot'
 
 
-                //.... process meal cost
-                //prent days x meal cost = meal cost deduction
-                //insert into payroll_pre_salary_sheet_deductions table
+
 
                 $total_present_days = 1; //todo::
                 $absent_days = 0; //todo::
-                $late_days = 0; //todo::
-                $deductable_late_days = 0; //todo::
+                $late_days = $lateAttendanceRecord->total_records ?? 0; //todo::
+                $deductable_late_days = $lateAttendanceRecord->total_records ?? 0; //todo::
 
-
+                //.... process meal cost
+                //prent days x meal cost = meal cost deduction
+                //insert into payroll_pre_salary_sheet_deductions table
                 if($total_present_days > 0 && $user->hasOfficialInformation->is_mealable == 1)
                 {
                     $total_meal_cost_this_month = $total_present_days * $user->hasOfficialInformation->per_meal_cost;
@@ -268,6 +269,31 @@ class ProcessTempSalaryJob extends Job implements ShouldQueue
                     }
 
                 }
+
+
+
+                //.... process and gather other regular salary components(like basic salary, allowances, deductions(attendance related), etc.)
+                    //get Payroll salary heads ( individual employee employee salary heads like: basic, medical, House rent etc)
+                    //.... get salary components data of this employee
+                    $head_amounts = SalaryHead::select('payroll_salary_heads.amount','salary_heads.id','payroll_salary_heads.is_earning')
+                                    ->leftJoin('payroll_salary_heads', 'payroll_salary_heads.salary_head_id', '=', 'salary_heads.id')
+                                    ->where('payroll_salary_heads.employee_user_id', $user->id)
+                                    ->get();
+
+                    // loop through each head
+                    //.... insert salary to TempSalary
+                    foreach($head_amounts as $head)
+                    {
+                        $PayrollSalarySheetHeads[] = [
+                            'salary_sheet_temp_id' => $this->payload['salary_sheet_temp_id'] ?? null,
+                            'head_id' => $head->id,
+                            'value' => $head->amount,
+                            'is_earning' => $head->is_earning,
+                            'created_user_id' => 1, // todo:: need a system user id
+                            'created_at' => date('Y-m-d H:i:s'),
+                        ];
+                    }
+
             }
         );
 
--- app/Models/EmployeeLeaveBalance.php
+++ app/Models/EmployeeLeaveBalance.php
@@ -9,4 +9,31 @@ class EmployeeLeaveBalance extends Model
     protected $table = 'employee_leave_balances';
     protected $primaryKey = 'id';
     protected $timestamp = false;
+    protected $fillable = [
+        'employee_user_id',
+        'leave_head_id',
+        'leave_policy_id',
+        'forwarded',
+        'achived_this_year',
+        'used',
+        'current_balance',
+        'encashed_balance',
+        'valid_until',
+        'child_data_identifier_key_incoming',
+        'child_data_identifier_key_outgoing',
+        'fiscal_year',
+        'created_user_id',
+        'updated_user_id',
+        'created_at',
+        'updated_at',
+        'deleted_user_id',
+        'deleted_by',
+        'deleted_at',
+    ];
+
+    public function hasLeaveHead()
+    {
+        return $this->hasOne(LeaveHead::class, 'id', 'leave_head_id');
+    }
+        
 }
